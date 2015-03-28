ffe98be688313a4f3ad6003272031d0b90ee8fac
megastupidmonkey <hh258hh@yahoo.com>
2015-03-28 18:05:40 -0400

    something

--- src/test/TestMySQL.java
+++ src/test/TestMySQL.java
@@ -3,9 +3,9 @@ import database.DBDriver;
 
 public class TestMySQL {
 	public static void main(String[] args){
-		String url = null;
-		String username = null;
-		String password = null;
+		String url = "jdbc:mysql://dev.lypst.com/msg";
+		String username = "user";
+		String password = "QuRWhaKHqHcfEbdx";
 		DBDriver driver = new DBDriver(url, username, password);
 		System.out.println(driver);
 	}
